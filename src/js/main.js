b6aa4801b656e8f2dc178a0aa136aaa8bf280598
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-07-25 19:16:12 +0000

    feat: added description to object continets

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,26 @@
-const BASE_URL = "https://restcountries.com/v3.1/name/";
+const continents = [
+  { name: 'africa', image: '/public/img/continents/africa.PNG', description: 'A África é o berço da humanidade, rica em culturas, idiomas e biodiversidade, com paisagens que variam de desertos a florestas tropicais.' },
+  { name: 'south-america', image: '/public/img/continents/america-do-sul.PNG', description: 'A América do Sul é um continente localizado principalmente no hemisfério sul, conhecido por sua grande diversidade cultural, climática e natural. Sua principal característica é a vasta biodiversidade, concentrada especialmente na Floresta Amazônica.'  },
+  { name: 'north-america', image: '/public/img/continents/north-america.PNG', description: 'A América do Norte é marcada por grandes economias e diversidade geográfica, incluindo florestas, montanhas e desertos.'  },
+  { name: 'asia', image: '/public/img/continents/asia.PNG', description: 'A Ásia é o maior continente do mundo, tanto em área quanto em população, abrigando culturas milenares e potências tecnológicas modernas.'  },
+  { name: 'europe', image: '/public/img/continents/europe.PNG', description: 'A Europa é conhecida por sua rica história, diversidade cultural e influência global em áreas como arte, ciência e política.'  },
+  { name: 'oceania', image: '/public/img/continents/oceania.PNG', description: 'A Oceania é formada por ilhas e países como Austrália e Nova Zelândia, conhecida por paisagens naturais únicas e culturas indígenas.'  }
+];
 
-async function getCountries(name) {
-  const res = await fetch(`${BASE_URL}${name}`);
-  const data = await res.json();
-  console.log(data);
-}
+const container = document.querySelector('.filters-section');
 
-getCountries("chile");
+continents.forEach(continent => {
+  const card = document.createElement('div');
+  card.className += `continent-card ${continent.name}`;
+  card.innerHTML = `
+    <h2>${continent.name}</h2>
+    <p class="about-continent">${continent.description}</p>
+    <button class="btnRegions">Explorar</button>
+  `;
+  card.onclick = () => {
+    localStorage.setItem('selectedContinent', continent.name.toLowerCase());
+    window.location.href = 'continent.html';
+  };
+  container.appendChild(card)
+  
+});
