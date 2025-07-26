33bb84f6856f25f161b0efb8458aee15fbf16716
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-07-26 20:49:02 +0000

    fix: fixed address to img objects continents

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,31 @@
 const continents = [
   {
     name: "africa",
-    image: "/public/img/continents/africa.PNG",
+    image: "../../public/img/continents/africa.PNG",
     description:
       "A África é o berço da humanidade, rica em culturas, idiomas e biodiversidade, com paisagens que variam de desertos a florestas tropicais.",
   },
   {
     name: "americas",
-    image: "/public/img/continents/america.PNG",
+    image: "../../public/img/continents/america.PNG",
     description:
       "A América é um vasto continente dividido em três partes: América do Norte, Central e do Sul. É marcada por grande diversidade geográfica, cultural e econômica, abrigando florestas tropicais, desertos, cordilheiras e grandes centros urbanos. Da herança indígena aos impactos da colonização europeia, a América é um continente de contrastes e riqueza natural.",
   },
   {
     name: "asia",
-    image: "/public/img/continents/asia.PNG",
+    image: "../../public/img/continents/asia.PNG",
     description:
       "A Ásia é o maior continente do mundo, tanto em área quanto em população, abrigando culturas milenares e potências tecnológicas modernas.",
   },
   {
     name: "europe",
-    image: "/public/img/continents/europe.PNG",
+    image: "../../public/img/continents/europe.PNG",
     description:
       "A Europa é conhecida por sua rica história, diversidade cultural e influência global em áreas como arte, ciência e política.",
   },
   {
     name: "oceania",
-    image: "/public/img/continents/oceania.PNG",
+    image: "../../public/img/continents/oceania.PNG",
     description:
       "A Oceania é formada por ilhas e países como Austrália e Nova Zelândia, conhecida por paisagens naturais únicas e culturas indígenas.",
   },
