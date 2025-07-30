d35cb5fe1f1ad00fe46517df333d8de5755acbf4
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-07-30 16:53:16 +0000

    feat: add class from bootstrap to cards country

--- src/js/continent.js
+++ src/js/continent.js
@@ -1,16 +1,18 @@
 const continent = localStorage.getItem("selectedContinent");
 const container = document.getElementById("continentCountries");
+const nameContinent = document.querySelector(".nameContinent");
 
 fetch("https://restcountries.com/v3.1/region/" + continent)
   .then((res) => res.json())
   .then((data) => {
     data.forEach((country) => {
       const card = document.createElement("div");
-      card.className = "card-country";
+      card.className = `card-country col-sm-6`;
       card.innerHTML = `
         <img src="${country.flags.svg}" class="img-card" alt="${country.name.common}" />
-        <h2>${country.name.common}</h2>
+        <h2 class="teste2">${country.name.common}</h2>
       `;
+      nameContinent.innerHTML = `${continent.toLocaleUpperCase()}`;
       card.onclick = () => {
         localStorage.setItem("selectedCountry", country.cca3);
         window.location.href = "country.html";
