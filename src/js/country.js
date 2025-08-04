36d2f2a27505d5381cd625b5fbe23ce9f5fbdc70
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-08-04 17:18:45 +0000

    feat: created and mov div to borders and card wrap

--- src/js/country.js
+++ src/js/country.js
@@ -8,81 +8,92 @@ fetch(`https://restcountries.com/v3.1/alpha/${code}`)
   .then((data) => {
     const country = data[0];
     container.innerHTML = `
+    <h2>${country.name.common}</h2>
       <div class="card">
-        <div class="card-flag">
-        <h2>${country.name.common}</h2>
-        <img src="${country.flags.svg}" alt="${country.name.common}" />
-        </div>
-        
-        <div class="country-infos">
-        <p><strong>Capital:</strong> ${country.capital?.[0] || "Não possui"}</p>
-        <p><strong>População:</strong> ${country.population.toLocaleString()}</p>
-        <p><strong>Região:</strong> ${country.region}</p>
-        <p><strong>Idioma:</strong> ${Object.values(
-          country.languages || {}
-        ).join(", ")}</p>
-        <p><strong>Lado da direção do carro:</strong> ${
-          country.car.side == "right" ? "Esquerdo" : "Direito"
-        }</p>
-        <p><strong>Independente:</strong> ${
-          country.independent ? "Sim" : "Não"
-        }</p>
-        <p><strong>Fusos horários:</strong> ${country.timezones}</p>
-        <div class="flag-coatOfArms">
-        <p><strong>Brasão:</strong> 
-  ${
-    country.coatOfArms.png
-      ? `<img src="${country.coatOfArms.png}" alt="coatOfArms" />`
-      : "Não informado"
-  }
-</p>
+        <div class="card-wrap">
+          <div class="card-flag">
+          <P><strong>Bandeira:</strong></p>         
+            <img src="${country.flags.svg}" alt="${country.name.common}" />
+          </div>           
+              <div class="flag-coatOfArms">
+              <P><strong>Brasão:</strong></p>                        
+                    ${
+                      country.coatOfArms.png
+                        ? `<img src="${country.coatOfArms.png}" alt="coatOfArms" />`
+                        : "Não informado"
+                    }                  
+              </div>              
+              </div>
+              <div class="country-infos">
+                <p><strong>Capital:</strong> ${
+                  country.capital?.[0] || "Não possui"
+                }</p>
+                <p><strong>População:</strong> ${country.population.toLocaleString()}</p>
+                <p><strong>Região:</strong> ${country.region}</p>
+                <p><strong>Idioma:</strong> ${Object.values(
+                  country.languages || {}
+                ).join(", ")}</p>
+                <p><strong>Lado da direção do carro:</strong> ${
+                  country.car.side == "right" ? "Esquerdo" : "Direito"
+                }</p>
+                <p><strong>Independente:</strong> ${
+                  country.independent ? "Sim" : "Não"
+                }</p>
+                <p><strong>Fusos horários:</strong> ${
+                  country.timezones
+                }</p>          
+              </div>        
+              
         </div>
       </div>
-      
-      <div class="borders">
+
       <h2>Fronteiras</h2>
-      </div>
+        <div class="borders">         
+        </div>
       
       <div class="suggestions">
-      <h2>Conheça também:</h2>
+        <h2>Conheça também:</h2>
       </div>
     `;
 
     if (country.borders?.length) {
-  fetch(
-    `https://restcountries.com/v3.1/alpha?codes=${country.borders.join(",")}`
-  )
-    .then((res) => res.json())
-    .then((borders) => {
-      const borderContainer = document.querySelector(".borders");
+      fetch(
+        `https://restcountries.com/v3.1/alpha?codes=${country.borders.join(
+          ","
+        )}`
+      )
+        .then((res) => res.json())
+        .then((borders) => {
+          const borderContainer = document.querySelector(".borders");
 
-      if (borders.length === 0) {
-        borderContainer.textContent = "Não possui fronteiras";
-        return;
-      }
+          if (borders.length === 0) {
+            borderContainer.textContent = "Não possui fronteiras";
+            return;
+          }
 
-      borders.forEach((border) => {
-        const div = document.createElement("div");
-        div.className = "card";
-        div.innerHTML = `
-        <h3>${border.name.common}</h3>
+          borders.forEach((border) => {
+            const div = document.createElement("div");
+            div.className = "card-borders";
+            div.innerHTML = `       
           <img src="${border.flags.svg}" alt="${border.name.common}" />
+          <h3>${border.name.common}</h3>
           
         `;
-        div.onclick = () => {
-          localStorage.setItem("selectedCountry", border.cca3);
-          location.reload();
-        };
-        borderContainer.appendChild(div);
-      });
-    })
-    .catch((err) => {
-      console.error("Erro ao buscar países vizinhos:", err);
-      document.querySelector(".borders").textContent = "Erro ao carregar fronteiras.";
-    });
-} else {
-  document.querySelector(".borders").textContent = "Não possui fronteiras";
-}
+            div.onclick = () => {
+              localStorage.setItem("selectedCountry", border.cca3);
+              location.reload();
+            };
+            borderContainer.appendChild(div);
+          });
+        })
+        .catch((err) => {
+          console.error("Erro ao buscar países vizinhos:", err);
+          document.querySelector(".borders").textContent =
+            "Erro ao carregar fronteiras.";
+        });
+    } else {
+      document.querySelector(".borders").textContent = "Não possui fronteiras";
+    }
 
     fetch("https://restcountries.com/v3.1/all?fields=name,flags,region,cca3")
       .then((res) => res.json())
