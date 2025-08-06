ccd015635a696cf6abb6649a8fa6d9f8a4509b7f
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-08-06 19:00:19 +0000

    feat: countries organized in alphabetical order

--- src/js/continent.js
+++ src/js/continent.js
@@ -5,14 +5,21 @@ const nameContinent = document.querySelector(".nameContinent");
 fetch("https://restcountries.com/v3.1/region/" + continent)
   .then((res) => res.json())
   .then((data) => {
+     
+    data.sort((a, b) =>
+      a.name.common.localeCompare(b.name.common)
+    );
+
+    
+    nameContinent.textContent = continent.toUpperCase();
+    
     data.forEach((country) => {
       const card = document.createElement("div");
-      card.className = `card-country col-sm-6`;
+      card.className = "card-country col-sm-6";
       card.innerHTML = `
         <img src="${country.flags.svg}" class="img-card" alt="${country.name.common}" />
-        <h3>${country.name.common}</h2>
+        <h3>${country.name.common}</h3>
       `;
-      nameContinent.innerHTML = `${continent.toLocaleUpperCase()}`;
       card.onclick = () => {
         localStorage.setItem("selectedCountry", country.cca3);
         window.location.href = "country.html";
